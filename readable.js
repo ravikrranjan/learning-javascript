eb1abd305d687c686c024584e529e1634389e060
ravikrranjan <raviranjan1600@gmail.com>
2020-05-03 18:33:45 +0530

    readable stream

--- readable.js
+++ readable.js
@@ -3,9 +3,23 @@ const {
 } = require('stream');
 
 
-const inStream = new Readable();
+// const inStream = new Readable();
 
-inStream.push('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
-inStream.push(null);
+// inStream.push('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+// inStream.push(null);
 
+
+const inStream = new Readable({
+    read(size) {
+        setTimeout(() => {
+            if (this.currentCharCode > 90) {
+                this.push(null);
+                return;
+            }
+            this.push(String.fromCharCode(this.currentCharCode++))
+
+        }, 100);
+    }
+})
+inStream.currentCharCode = 65;
 inStream.pipe(process.stdout);
