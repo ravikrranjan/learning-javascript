722681b8465d4e54ed90da69be11982c900f15bf
ravikrranjan <raviranjan1600@gmail.com>
2020-05-03 13:31:52 +0530

    event emitter

---
+++ event_emitter.js
@@ -0,0 +1,35 @@
+const EventEmitter = require('events');
+
+// console.log(EventEmitter);
+
+
+class WithLog extends EventEmitter {
+
+    execute(taskFunc) {
+
+        console.log('Before executing');
+        this.emit('begin');
+        taskFunc();
+        this.emit('end');
+        console.log('After executing');
+
+
+    }
+
+}
+
+const withLog = new WithLog();
+
+withLog.on('begin', () => {
+    console.log(`About to execute`);
+});
+
+
+withLog.on('end', () => {
+    console.log('Done with execute');
+});
+
+withLog.execute(() => {
+    console.log('**** Executing task ****');
+
+})
