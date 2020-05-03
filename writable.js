26bc824162a0457e6e1e99ccb6a5460f9d036547
ravikrranjan <raviranjan1600@gmail.com>
2020-05-03 18:15:11 +0530

    stream writable stdout

--- writable.js
+++ writable.js
@@ -1,12 +1,15 @@
-const {
-    Writable
-} = require('stream');
+// const {
+//     Writable
+// } = require('stream');
 
-const outStream = new Writable({
-    write(chunk, encoding, callback) {
-        console.log(chunk.toString());
-        callback();
-    }
-});
+// const outStream = new Writable({
+//     write(chunk, encoding, callback) {
+//         console.log(chunk.toString());
+//         callback();
+//     }
+// });
 
-process.stdin.pipe(outStream);
+// process.stdin.pipe(outStream);
+
+
+process.stdin.pipe(process.stdout)
